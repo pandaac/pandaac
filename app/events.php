480ca7dbe5e352ab434baaba030d0ea84defdd4c
Christopher <eklundchristopher@gmail.com>
2014-09-10 01:06:36 +0200

    Initial Commit

---
+++ app/events.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| 7 days of free premium
+|--------------------------------------------------------------------------
+|
+| Assign 7 days of free premium to every single new account.
+|
+*/
+
+// Event::listen('account.created', function($account)
+// {
+// 	$account->addPremiumDays(7);
+// 	$account->save();
+// });
---
+++ app/themes/default/layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+
+	<title>pandaac</title>
+</head>
+<body>
+
+	...
+
+</body>
+</html>
