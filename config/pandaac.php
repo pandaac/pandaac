92bb169c26d7b7a99d2a57cb489fbe0ae973af92
Christopher Eklund <eklundchristopher@gmail.com>
2015-06-13 21:16:59 +0200

    Publish configs

--- config/pandaac.php
+++ config/pandaac.php
@@ -11,20 +11,22 @@ return [
     | request to your application. Feel free to add your own services to
     | this array to grant expanded functionality to your applications.
     |
+    | By default, we're loading the Apolune\ApoluneServiceProvider which in
+    | turn registers each Apolune component indepedently. Alternatively,
+    | you may load only the Apolune components you see a need for.
+    |
     */
 
     'providers' => [
-
         'Apolune\ApoluneServiceProvider',
-
     ],
 
     /*
     |--------------------------------------------------------------------------
-    | Theme
+    | Theme Service Provider
     |--------------------------------------------------------------------------
     |
-    | ...
+    | Specify the Service Provider for the theme you would like to utilise.
     |
     */
 
