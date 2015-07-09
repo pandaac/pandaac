5e24d51bab52991570036c63b11e1cfa0f78b87c
Christopher Eklund <eklundchristopher@gmail.com>
2015-07-09 21:51:59 +0200

    Timers config

---
+++ config/pandaac/timers.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Email Address Change Timer
+    |--------------------------------------------------------------------------
+    |
+    | Enter the amount of days you would like the user to wait before their
+    | requested email address change goes through.
+    |
+    */
+
+    'email-change' => 10,
+
+];
