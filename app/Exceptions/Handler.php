74b995b151dd5e9a960beb27cf2145fd90f7b19c
Christopher Eklund <eklundchristopher@gmail.com>
2015-08-21 18:41:17 +0200

    Exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,6 @@
 namespace App\Exceptions;
 
 use Exception;
-use Apolune\Profile\Exceptions\NotFoundPlayerException;
 use Apolune\Core\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
@@ -40,39 +39,6 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        // if ($this->isHttpException($e)) {
-        //     return redirect('/');
-        // }
-
-        if ($e instanceof NotFoundPlayerException) {
-            $player = app('router')->getCurrentRoute()->getParameter('player');
-
-            return redirect('/characters')->with('name', $player);
-        }
-
-        if (config('app.debug')) {
-            return $this->renderExceptionWithWhoops($e);
-        }
-
         return parent::render($request, $e);
-    }
-
-    /**
-     * Render an exception using Whoops.
-     *
-     * @param  \Exception $e
-     * @return \Illuminate\Http\Response
-     */
-    protected function renderExceptionWithWhoops(Exception $e)
-    {
-        $whoops = new \Whoops\Run;
-        
-        $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
-
-        return new \Illuminate\Http\Response(
-            $whoops->handleException($e),
-            $e->getStatusCode(),
-            $e->getHeaders()
-        );
     }
 }
