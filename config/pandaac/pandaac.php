ae739ab682d6cbd258982be27eae6f151d1563d4
Christopher Eklund <eklundchristopher@gmail.com>
2015-09-03 23:19:06 +0200

    Ignore config/pandaac directories, but not files

---
+++ config/pandaac/pandaac.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Autoloaded Service Providers
+    |--------------------------------------------------------------------------
+    |
+    | The service providers listed here will be automatically loaded on the
+    | request to your application. Feel free to add your own services to
+    | this array to grant expanded functionality to your applications.
+    |
+    | By default, we're loading the Apolune\ApoluneServiceProvider which in
+    | turn registers each Apolune component indepedently. Alternatively,
+    | you may load only the Apolune components you see a need for.
+    |
+    */
+
+    'providers' => [
+        Apolune\ApoluneServiceProvider::class,
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Theme Service Provider
+    |--------------------------------------------------------------------------
+    |
+    | Specify the Service Provider for the theme you would like to utilise.
+    |
+    */
+
+    'theme' => pandaac\ThemeTibia\Providers\ThemeServiceProvider::class,
+
+];
